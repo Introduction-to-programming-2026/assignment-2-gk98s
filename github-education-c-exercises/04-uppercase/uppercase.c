03d6a87b3b9b812bd6595ccd9ea94fb7116207a6
gk98s <89647115+gk98s@users.noreply.github.com>
2026-03-06 16:27:27 +0000

    implemented uppercase

--- github-education-c-exercises/04-uppercase/uppercase.c
+++ github-education-c-exercises/04-uppercase/uppercase.c
@@ -1,14 +1,22 @@
-#include <cs50.h>
+//#include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
-    string s = get_string("Before: ");
+    char s[128];
+
+    printf("Before: ");
+    scanf("%s", s);
+    printf("%s\n", s);
+
     printf("After: ");
 
-    // TODO: loop through the string and print toupper(...) for each char
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", toupper(s[i]));
+    }
 
     printf("\n");
 }
